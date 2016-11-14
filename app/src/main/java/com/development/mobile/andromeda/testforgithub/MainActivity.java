3222e81d60aa729d79ca45532655aa1885afa348
AndromedaMobileDevelopment <kerjen01@gmail.com>
2016-11-14 19:17:57 +0300

    бблабла версия 787593

--- app/src/main/java/com/development/mobile/andromeda/testforgithub/MainActivity.java
+++ app/src/main/java/com/development/mobile/andromeda/testforgithub/MainActivity.java
@@ -10,11 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        String Hello = "Wrol!Rghihwg";
-        String Hello1;
-        String kuku;
-        String HEEELELHSKSGKSGJsGK;
-        String kaka;
+        String Hello;
     }
 }
